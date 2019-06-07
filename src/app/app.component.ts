b4f0e13b772a775709df4702187db884f5b75ebb
solairajesh <solairajesh@gmail.com>
2019-06-07 20:07:03 +0530

    4 files modified - added dummy carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  sliders = [{imagePath:'https://rawgit.com/start-angular/SB-Admin-BS4-Angular-6/master/dist/assets/images/slider2.jpg', label:'Car label', text:'Car text'},{imagePath:'https://rawgit.com/start-angular/SB-Admin-BS4-Angular-6/master/dist/assets/images/slider2.jpg', label:'Car label2', text:'Car text2'},{imagePath:'https://rawgit.com/start-angular/SB-Admin-BS4-Angular-6/master/dist/assets/images/slider2.jpg', label:'Car label3', text:'Car text3'}];
 }
