6b59a355ef398f3ae8b9e70e04d31d93c4294306
Solai Rajesh <solairajesh@gmail.com>
2019-06-10 08:48:53 +0530

    added defaul cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  sliders = [{imagePath:'https://rawgit.com/start-angular/SB-Admin-BS4-Angular-6/master/dist/assets/images/slider2.jpg', label:'Car label', text:'Car text'},{imagePath:'https://rawgit.com/start-angular/SB-Admin-BS4-Angular-6/master/dist/assets/images/slider2.jpg', label:'Car label2', text:'Car text2'},{imagePath:'https://rawgit.com/start-angular/SB-Admin-BS4-Angular-6/master/dist/assets/images/slider2.jpg', label:'Car label3', text:'Car text3'}];
+export class AppComponent {
+  title = 'angular7-demo';
 }
