e9bfee56ea5ef2c56a4427992b1215c24bc4a9bb
solairajesh <solairajesh@gmail.com>
2019-06-10 12:50:21 +0530

    3 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header-component/header.component';
+import { FooterComponent } from './footer-component/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HeaderComponent, FooterComponent
   ],
   imports: [
     BrowserModule
