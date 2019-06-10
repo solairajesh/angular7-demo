8f08dabc7f17737b8afdee80d79554b2b725a580
solairajesh <solairajesh@gmail.com>
2019-06-10 22:13:30 +0530

    2 files modified, 12 added, 6 deleted - added home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header-component/header.component';
-import { FooterComponent } from './footer-component/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { FeaturedProductComponent } from './featured-product/featured-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, FooterComponent
+    AppComponent, HeaderComponent, FooterComponent, BannerComponent, FeaturedProductComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/featured-product/featured-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'featured-product',
+  templateUrl: './featured-product.component.html',
+  styleUrls: ['./featured-product.component.css']
+})
+export class FeaturedProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
